63d37f71d9d83c0e595535eac3bca67314020065
ayushcodes0 <ayushsinghcodes@gmail.com>
2025-07-27 17:51:35 +0530

    navbar completed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,8 +11,8 @@ const Navbar = () => {
         <a href="#contact" className={Styles.navLink}>Contact</a>
       </div>
       <div className={Styles.buttons}>
-        <UnfilledButton text="Login" />
-        <FilledButton text="Get Started" />
+        <UnfilledButton text="Log in" />
+        <FilledButton text="Get started" />
       </div>
     </div>
   )
