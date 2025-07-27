ab3d31143504c58c16649def0ace699a2328391c
ayushcodes0 <ayushsinghcodes@gmail.com>
2025-07-28 01:25:34 +0530

    working on hero section

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,14 +1,56 @@
+import { FilledButton } from '../../components/Button/Button'
 import Navbar from '../../components/Navbar/Navbar'
 import TopBanner from '../../components/TopBanner/TopBanner'
 import Styles from './LandingPage.module.css'
+import { IoFolderOpenOutline } from "react-icons/io5";
+import { TbPlant } from "react-icons/tb";
+
+
 const LandingPage = () => {
   return (
     <div className={Styles.landingPageContainer}>
-      <div className={Styles.topBannerContainer}>
-        <TopBanner/>
-      </div>
-      <div className={Styles.navbarContainer}>
-        <Navbar/>
+      <div className={Styles.landingPageHero}>
+        <div className={Styles.topBannerContainer}>
+          <TopBanner/>
+        </div>
+        <div className={Styles.navbarContainer}>
+          <Navbar/>
+        </div>
+        <div className={Styles.heroSection}>
+          <div className={Styles.heroSectionLeft}>
+            <div className={Styles.mainHeading}>
+              <p className={Styles.topHeading}>Money works <br />better here.</p>
+              <div className={Styles.middleHeading}>
+                <div className={Styles.middleHeadingLeft}>
+                  <div className={Styles.middleHeadingIcon}><IoFolderOpenOutline className={Styles.folderIcon}/></div>
+                  <div className={Styles.middleHeadingText}>
+                    <p className={Styles.middleSubHeading}>Lorem, ipsum dolor.</p>
+                    <p className={Styles.middleSubText}>Lorem ipsum dolor sit amet consectetur.</p>
+                  </div>
+                </div>
+                <div className={Styles.middleHeadingRight}>
+                  <div className={Styles.middleHeadingIcon}><TbPlant className={Styles.folderIcon}/></div>
+                  <p className={Styles.middleSubHeading}>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                </div>
+              </div>
+              <div className={Styles.ctaContainer}>
+                <div className={Styles.ctaButton}><FilledButton text='Get started'/></div>
+                <p className={Styles.ctaText}>Lorem ipsum dolor sit.</p>
+              </div>
+            </div>
+            <div className={Styles.headingBottom}>
+              <div className={Styles.bottomLeft}>
+                <p className={Styles.bottomHeading}>Lorem.</p>
+                <p className={Styles.bottomSubHeading}>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+              <div className={Styles.bottomRight}>
+                <p className={Styles.bottomHeading}>Lorem.</p>
+                <p className={Styles.bottomSubHeading}>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </div>
+          </div>
+          <div className={Styles.heroSectionRight}></div>
+        </div>
       </div>
     </div>
   )
