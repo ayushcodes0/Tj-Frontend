53be106dd1aec371abf9950a4f7efb3761a1d1be
ayushcodes0 <ayushsinghcodes@gmail.com>
2025-07-30 18:29:15 +0530

    Footer Done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,10 @@
 import Styles from './Footer.module.css';
 const Footer = () => {
   return (
-    <div className={Styles.footer}>
+    <div className={Styles.footerWrapper}>
+      <div className={Styles.footer}>
       <div className={Styles.footerLogo}>
-        <p className={Styles.logo}>LoremIpsum</p>
+        <p className={Styles.logo}>LoremIpsum</p> 
       </div>
       <div className={Styles.footerContent}>
         <div className={Styles.footerItems}>
@@ -34,6 +35,10 @@ const Footer = () => {
             <p className={Styles.footerItemsText}>Lorem, ipsum dolor.</p>
             <p className={Styles.footerItemsText}>lorem</p>
         </div>
+      </div>
+    </div>
+      <div className={Styles.footerBottom}>
+        <p className={Styles.footerBottomText}>© 2023 LoremIpsum. All rights reserved.</p>
       </div>
     </div>
   )
