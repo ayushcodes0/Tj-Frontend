8266c0ddf6e748f54e2b3d88d03779949fe014cc
ayushcodes0 <ayushsinghcodes@gmail.com>
2025-07-30 22:57:50 +0530

    login page background added

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,24 @@
+import Styles from './Login.module.css';
 
 const Login = () => {
   return (
-    <div>
-      Login page
+    <div className={Styles.loginPageContainer}>
+      {/* Enhanced blurry balls background */}
+      <div className={Styles.blurryBalls}>
+        <div className={Styles.ball1}></div>
+        <div className={Styles.ball2}></div>
+        <div className={Styles.ball3}></div>
+        <div className={Styles.ball4}></div>
+        <div className={Styles.ball5}></div>
+      </div>
+      
+      {/* Login content */}
+      <div className={Styles.loginContent}>
+        <h1>Welcome Back</h1>
+        {/* Your login form would go here */}
+      </div>
     </div>
   )
 }
 
-export default Login
+export default Login;
